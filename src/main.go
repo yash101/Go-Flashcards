a306eb7444350fbbeb738caf5a1a914fd899496e
Dev Lodha <lodhad@gmail.com>
2016-11-26 18:58:05 -0700

    Working on figuring out web. Trying to push to switch platforms

---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import(
+	"fmt"
+	"net/http"
+)
+
+func handlerRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "[ResponseWriter]:{\n%+v\n}\n[Request]:{\n%+v\n}\n", w, r)
+	fmt.Printf("[ResponseWriter]:{\n%+v\n}\n[Request]:{\n%+v\n}\n", w, r)
+}
+
+func main() {
+	http.HandleFunc("/", handlerRoot)
+	http.ListenAndServe(":8080", nil)
+}
